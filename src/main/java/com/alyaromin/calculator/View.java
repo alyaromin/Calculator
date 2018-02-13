fb27a1fd459cdbed796fb8367805ff5a1aa5acbf
alyaromin <alyaromin@gmail.com>
2018-02-14 00:07:15 +0300

    clear window console

--- src/main/java/com/alyaromin/calculator/View.java
+++ src/main/java/com/alyaromin/calculator/View.java
@@ -15,7 +15,17 @@ public class View {
     }
 
     private void clearConsole() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+        try {
+            final String os = System.getProperty("os.name");
+
+            if (os.contains("Windows")) {
+                Runtime.getRuntime().exec("cls");
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
     }
 }
